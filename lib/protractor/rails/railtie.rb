f1e1b4083c4ddcc34000c266b60ec2409324ecea
Tyrone <tdubs241083@gmail.com>
2014-03-19 21:35:57 +0200

    load the protractor rake file for a rails app

--- lib/protractor/rails/railtie.rb
+++ lib/protractor/rails/railtie.rb
@@ -3,6 +3,7 @@ module Protractor
     class Railtie < ::Rails::Railtie
       rake_tasks do
         load File.expand_path('../../../tasks/install.rake', __FILE__)
+        load File.expand_path('../../../tasks/protractor.rake', __FILE__)
       end
     end
   end
